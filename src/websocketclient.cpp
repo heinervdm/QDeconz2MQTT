cd6d78fadd5a43429e6f64f961aa10cedf0f8f51
Thomas Zimmermann <thomas.zimmermann@voestalpine.com>
2026-01-30 10:31:31 +0100

    Add debug message to check current cache state

--- src/websocketclient.cpp
+++ src/websocketclient.cpp
@@ -19,62 +19,61 @@
 
 #include "websocketclient.h"
 
-#include <QTextStream>
 #include <QJsonDocument>
+#include <QTextStream>
 
-WebSocketClient::WebSocketClient(const Deconz2MQTTConfig &config, QObject *parent)
-    : QObject(parent)
-{
-    connect(&m_webSocket, &QWebSocket::disconnected, this, &WebSocketClient::closed);
-    connect(&m_webSocket, &QWebSocket::textMessageReceived, this, &WebSocketClient::onTextMessageReceived);
-    m_webSocket.open(config.websocketUrl());
+WebSocketClient::WebSocketClient(const Deconz2MQTTConfig &config,
+                                 QObject *parent)
+    : QObject(parent) {
+  connect(&m_webSocket, &QWebSocket::disconnected, this,
+          &WebSocketClient::closed);
+  connect(&m_webSocket, &QWebSocket::textMessageReceived, this,
+          &WebSocketClient::onTextMessageReceived);
+  m_webSocket.open(config.websocketUrl());
 }
 
-void WebSocketClient::onTextMessageReceived(const QString &message)
-{
-    QTextStream(stdout) << "Websocket message received: " << message << Qt::endl;
+void WebSocketClient::onTextMessageReceived(const QString &message) {
+  QTextStream(stdout) << "Websocket message received: " << message << Qt::endl;
 
-    QJsonParseError err;
-    QJsonDocument doc = QJsonDocument::fromJson(message.toUtf8(), &err);
-    if (!doc.isNull())
-    {
-        QVariant var = doc.toVariant();
-        if (var.type() == QVariant::Map)
-        {
-            QVariantMap map = var.toMap();
-            QString role = map.value("r").toString();
-            QString t = map.value("t").toString();
-            QString event = map.value("e").toString();
-            if (event == "changed" && t == "event" && role == "sensors")
-            {
-                const QString uniqueid = map.value("uniqueid").toString();
-                if (!uniqueid.isEmpty())
-                {
-                    m_latestdata[uniqueid].insert(map);
-                    const QVariantMap attr = m_latestdata[uniqueid].value("attr").toMap();
-                    const QString type = attr.value("type").toString();
-                    if (!type.isEmpty())
-                    {
-                        emit messageReceived(uniqueid, type, m_latestdata[uniqueid]);
-                    }
-                }
-                else
-                {
-                    QTextStream(stderr) << "Json error: message does not contain a uniqueid" << Qt::endl;
-                }
-            }
-            else
-            {
-                QTextStream(stderr) << "Json error: message does not contain: r = sensors, t = event and e = changed" << Qt::endl;
-            }
+  QJsonParseError err;
+  QJsonDocument doc = QJsonDocument::fromJson(message.toUtf8(), &err);
+  if (!doc.isNull()) {
+    QVariant var = doc.toVariant();
+    if (var.type() == QVariant::Map) {
+      QVariantMap map = var.toMap();
+      QString role = map.value("r").toString();
+      QString t = map.value("t").toString();
+      QString event = map.value("e").toString();
+      if (event == "changed" && t == "event" && role == "sensors") {
+        const QString uniqueid = map.value("uniqueid").toString();
+        if (!uniqueid.isEmpty()) {
+          m_latestdata[uniqueid].insert(map);
+          QTextStream(stdout)
+              << "Websocket current data: "
+              << QJsonDocument::fromVariant(m_latestdata[uniqueid])
+                     .toJson(QJsonDocument::Compact)
+              << Qt::endl;
+          const QVariantMap attr = m_latestdata[uniqueid].value("attr").toMap();
+          const QString type = attr.value("type").toString();
+          if (!type.isEmpty()) {
+            emit messageReceived(uniqueid, type, m_latestdata[uniqueid]);
+          }
+        } else {
+          QTextStream(stderr)
+              << "Json error: message does not contain a uniqueid" << Qt::endl;
         }
-        else
-        {
-            QTextStream(stderr) << "Json error: message is not a QVariantMap" << Qt::endl;
-        }
+      } else {
+        QTextStream(stderr) << "Json error: message does not contain: r = "
+                               "sensors, t = event and e = changed"
+                            << Qt::endl;
+      }
+    } else {
+      QTextStream(stderr) << "Json error: message is not a QVariantMap"
+                          << Qt::endl;
     }
-    else
-    {
-        QTextStream(stderr) << "Json error: error while parsing message: " << message << ", Error message: " << err.errorString() << Qt::endl;
-    }
+  } else {
+    QTextStream(stderr) << "Json error: error while parsing message: "
+                        << message << ", Error message: " << err.errorString()
+                        << Qt::endl;
+  }
 }
